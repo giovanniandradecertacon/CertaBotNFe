74e9aad267c959f7df59c770974e5d7a5db8779f
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-24 16:39:10 -0300

    feat: criação de service para pagina de login

--- src/main/java/br/com/certacon/certabotnfefiles/services/LoginTabService.java
+++ src/main/java/br/com/certacon/certabotnfefiles/services/LoginTabService.java
@@ -1,7 +1,11 @@
 package br.com.certacon.certabotnfefiles.services;
 
+import br.com.certacon.certabotnfefiles.helpers.SeleniumHelperComponent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoginTabService {
+    private final SeleniumHelperComponent helper;
 }
