169ed72750eb906aefe0b924dd85a47b05c6b5de
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-24 16:39:37 -0300

    test: criação de teste LoginTabService

---
+++ src/test/java/br/com/certacon/certabotnfefiles/services/LoginTabServiceTest.java
@@ -0,0 +1,17 @@
+package br.com.certacon.certabotnfefiles.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginTabServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
