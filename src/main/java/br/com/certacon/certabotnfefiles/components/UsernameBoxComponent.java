7bfcd5fecbddba09ccd0405bc6d0a77ea4a2fe72
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-24 15:56:02 -0300

    fix: correção de componente

--- src/main/java/br/com/certacon/certabotnfefiles/components/UsernameBoxComponent.java
+++ src/main/java/br/com/certacon/certabotnfefiles/components/UsernameBoxComponent.java
@@ -1,8 +1,7 @@
 package br.com.certacon.certabotnfefiles.components;
 
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserNameBoxComponent {
+public class UsernameBoxComponent {
 }
