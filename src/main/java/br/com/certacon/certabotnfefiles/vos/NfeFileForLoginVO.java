903346516a2fed3b70701f00d6a4b623c2aebd1f
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-25 17:40:27 -0300

    chore: adição de propriedade de id

--- src/main/java/br/com/certacon/certabotnfefiles/vos/NfeFileForLoginVO.java
+++ src/main/java/br/com/certacon/certabotnfefiles/vos/NfeFileForLoginVO.java
@@ -1,16 +1,29 @@
 package br.com.certacon.certabotnfefiles.vos;
 
+import br.com.certacon.certabotnfefiles.models.NfeFileModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @Data
 @Builder
 public class NfeFileForLoginVO {
+    private UUID id;
     private String username;
 
     private String password;
 
     private String remoteDriverUpload;
+
+    public NfeFileModel toModel(){
+        return NfeFileModel.builder()
+                .id(id)
+                .username(username)
+                .password(password)
+                .remoteDriverUpload(remoteDriverUpload)
+                .build();
+    }
 }
