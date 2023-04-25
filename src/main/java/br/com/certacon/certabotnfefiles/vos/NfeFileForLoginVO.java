15b22ef1812165faf34ec27d164fa7e0221d7bcf
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-25 11:31:26 -0300

    chore: adição de propriedades para VO de Login

--- src/main/java/br/com/certacon/certabotnfefiles/vos/NfeFileForLoginVO.java
+++ src/main/java/br/com/certacon/certabotnfefiles/vos/NfeFileForLoginVO.java
@@ -1,4 +1,16 @@
 package br.com.certacon.certabotnfefiles.vos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
 public class NfeFileForLoginVO {
+    private String username;
+
+    private String password;
+
+    private String remoteDriverUpload;
 }
