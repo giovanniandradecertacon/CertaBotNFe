49f995fe0899e3244563b56bfec3c29ac8271856
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-25 13:39:42 -0300

    test: criação de teste para main

--- src/test/java/br/com/certacon/certabotnfefiles/CertaBotNfeFilesApplicationTests.java
+++ src/test/java/br/com/certacon/certabotnfefiles/CertaBotNfeFilesApplicationTests.java
@@ -2,12 +2,13 @@ package br.com.certacon.certabotnfefiles;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CertaBotNfeFilesApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void main() { CertaBotNfeFilesApplication.main(new String[]{}); }
 
 }
