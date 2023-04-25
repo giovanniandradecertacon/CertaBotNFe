4db9741489a1c8e027817ec9ed9f21c11a544e1f
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-25 11:32:50 -0300

    chore: adição de metodos para encontrar elementos e passar chaves ou strings

--- src/main/java/br/com/certacon/certabotnfefiles/helpers/SeleniumHelperComponent.java
+++ src/main/java/br/com/certacon/certabotnfefiles/helpers/SeleniumHelperComponent.java
@@ -1,6 +1,8 @@
 package br.com.certacon.certabotnfefiles.helpers;
 
+import br.com.certacon.certabotnfefiles.utils.NfeStatus;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,40 +14,73 @@ import java.util.Optional;
 
 @Component
 public class SeleniumHelperComponent {
-    public Optional<WebElement> findElementByXpathWithSendKeys(Long sleepTime, Long wait, String xpath, String keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
-        Thread.sleep(sleepTime);
-        WebElement box = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
-        box.sendKeys(keys);
+    public Boolean navigateToUrl(RemoteWebDriver remoteWebDriver, String urlToNavigate){
+        Boolean navigated = null;
+        try{
+            remoteWebDriver.manage().window().maximize();
+            remoteWebDriver.navigate().to(urlToNavigate);
+            navigated = Boolean.TRUE;
+        }catch (RuntimeException e){
+            navigated = Boolean.FALSE;
+            throw new RuntimeException("Falha ao navegar pra URL passada");
+        }finally {
+            return navigated;
+        }
 
-        return Optional.ofNullable(box);
+    }
+
+    public Optional<WebElement> findElementByXpathAndSendKeysString(Long sleepTime, Long wait, String xpath, String keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
+        Thread.sleep(sleepTime);
+        WebElement sendBox = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        sendBox.sendKeys(keys);
+
+        return Optional.ofNullable(sendBox);
+    }
+
+    public Optional<WebElement> findElementByXpathAndSendKeys(Long sleepTime, Long wait, String xpath, Keys keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
+        Thread.sleep(sleepTime);
+        WebElement sendBox = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        sendBox.sendKeys(keys);
+
+        return Optional.ofNullable(sendBox);
     }
 
     public Optional<WebElement> findElementByIdWithSendKeys(Long sleepTime, Long wait, String id, String keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
         Thread.sleep(sleepTime);
-        WebElement box = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+        WebElement sendBox = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
-        box.sendKeys(keys);
+        sendBox.sendKeys(keys);
 
-        return Optional.ofNullable(box);
+        return Optional.of(sendBox);
     }
 
     public Optional<WebElement> findElementByClassNameWithSendKeys(Long sleepTime, Long wait, String className, String keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
         Thread.sleep(sleepTime);
-        WebElement box = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+        WebElement sendBox = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
                 .until(ExpectedConditions.presenceOfElementLocated(By.className(className)));
-        box.sendKeys(keys);
+        sendBox.sendKeys(keys);
 
-        return Optional.ofNullable(box);
+        return Optional.ofNullable(sendBox);
     }
 
     public Optional<WebElement> findElementByCssSelectorWithSendKeys(Long sleepTime, Long wait, String cssSelector, String keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
         Thread.sleep(sleepTime);
-        WebElement box = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+        WebElement sendBox = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(cssSelector)));
-        box.sendKeys(keys);
+        sendBox.sendKeys(keys);
 
-        return Optional.ofNullable(box);
+        return Optional.ofNullable(sendBox);
+    }
+
+    public Optional<WebElement> findElementByXpathToBeClickableAndClick(Long sleepTime, Long wait, String xpath, RemoteWebDriver remoteWebDriver) throws InterruptedException {
+        Thread.sleep(sleepTime);
+        WebElement clickBox = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+        clickBox.click();
+
+        return Optional.ofNullable(clickBox);
     }
 
 
