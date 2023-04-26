d3bfd6e1ee2da6b4eee455e3cc2786f2085f97a2
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-26 17:45:35 -0300

    fix: melhoramento de codigo

--- src/main/java/br/com/certacon/certabotnfefiles/pages/CertaconHomePage.java
+++ src/main/java/br/com/certacon/certabotnfefiles/pages/CertaconHomePage.java
@@ -1,7 +1,6 @@
 package br.com.certacon.certabotnfefiles.pages;
 
 import br.com.certacon.certabotnfefiles.helpers.SeleniumHelperComponent;
-import br.com.certacon.certabotnfefiles.repositories.NfeFileRepository;
 import br.com.certacon.certabotnfefiles.utils.NfeStatus;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -14,38 +13,35 @@ import java.util.Optional;
 @Service
 public class CertaconHomePage {
     private final SeleniumHelperComponent helper;
+
     public CertaconHomePage(SeleniumHelperComponent helper) {
         this.helper = helper;
     }
 
     public NfeStatus closeAndNavigate(RemoteWebDriver remoteWebDriver) throws MalformedURLException {
-
         NfeStatus homePageStatus = null;
 
         try {
             Optional<WebElement> tutorialElement = helper.findElementByXpathAndSendKeys(1000L, 30L, "/html/body/div[3]/div/div/button", Keys.ESCAPE, remoteWebDriver);
-            if(tutorialElement.isPresent()){
-                Optional<WebElement> filterFrameElement = helper.switchToFrameByClassName(1000L, 30L, "geo-smart-iframe", remoteWebDriver);
-                if(filterFrameElement.isPresent()){
+            if (tutorialElement.isPresent()) {
+                Optional<WebElement> frameElement = helper.switchToFrameByClassName(1000L, 30L, "geo-smart-iframe", remoteWebDriver);
+                if (frameElement.isPresent()) {
                     Optional<WebElement> filterElement = helper.findElementNotButtonByXpathAndClick(1000L, 30L, "/html/body/div[17]/div[1]/div/div/div/div/div[2]/div[4]/div/div", remoteWebDriver);
-                    if(filterElement.isPresent()){
-                        remoteWebDriver.switchTo().parentFrame();
-                        Optional<WebElement> sideMenuElement = helper.findElementByXpathAndClick(1000L, 30L, "/html/body/div[4]/div[1]/div/div/button", remoteWebDriver);
-                        if(sideMenuElement.isPresent()){
-                            Optional<WebElement> uploadFilesElement = helper.findElementNotButtonByXpathAndClick(1000L, 30L,"/html/body/div[4]/div[2]/div/div[1]/div/ul/li[4]/div/ul/li[1]/div/ul/li", remoteWebDriver);
-                            if(uploadFilesElement.isPresent()){
+                    if (filterElement.isPresent()) {
+                        Optional<WebElement> efdElement = helper.findElementNotButtonByXpathAndClick(1000L, 30L, "//*[@id=\"geoMenu_wrap\"]/li[2]/a", remoteWebDriver);
+                        if (efdElement.isPresent()) {
+                            Optional<WebElement> uploadFilesElement = helper.findElementNotButtonByXpathAndClick(1000L, 30L, "/html/body/div[6]/div[2]/div/div/div/div/div/div[2]/div/div/div/div[1]/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div/div/ul/li/a", remoteWebDriver);
+                            if (uploadFilesElement.isPresent()) {
                                 homePageStatus = NfeStatus.CHANGED;
                             }
                         }
-
                     }
                 }
-
             }
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             homePageStatus = NfeStatus.ERROR;
-            throw new RuntimeException(e.getMessage(), e.getCause());
-        }finally {
+            throw new RuntimeException();
+        } finally {
             return homePageStatus;
         }
     }
