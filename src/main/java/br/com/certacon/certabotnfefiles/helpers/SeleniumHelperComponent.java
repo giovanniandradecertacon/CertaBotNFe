4bceae86e27410474361d5025105a78f79248347
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-26 17:46:27 -0300

    chore: adição de métodos

--- src/main/java/br/com/certacon/certabotnfefiles/helpers/SeleniumHelperComponent.java
+++ src/main/java/br/com/certacon/certabotnfefiles/helpers/SeleniumHelperComponent.java
@@ -3,6 +3,7 @@ package br.com.certacon.certabotnfefiles.helpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,16 +14,16 @@ import java.util.Optional;
 
 @Component
 public class SeleniumHelperComponent {
-    public Boolean navigateToUrl(RemoteWebDriver remoteWebDriver, String urlToNavigate){
+    public Boolean navigateToUrl(RemoteWebDriver remoteWebDriver, String urlToNavigate) {
         Boolean navigated = null;
-        try{
+        try {
             remoteWebDriver.manage().window().maximize();
             remoteWebDriver.navigate().to(urlToNavigate);
             navigated = Boolean.TRUE;
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             navigated = Boolean.FALSE;
             throw new RuntimeException("Falha ao navegar pra URL passada");
-        }finally {
+        } finally {
             return navigated;
         }
 
@@ -42,7 +43,6 @@ public class SeleniumHelperComponent {
         WebElement clickSpace = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
                 .until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
         clickSpace.click();
-
         return Optional.ofNullable(clickSpace);
     }
 
@@ -53,6 +53,17 @@ public class SeleniumHelperComponent {
         clickSpace.click();
 
         return Optional.ofNullable(clickSpace);
+    }
+
+    public Optional<WebElement> findElementByXpathAndDoubleClick(Long sleepTime, Long wait, String xpath, RemoteWebDriver remoteWebDriver) throws InterruptedException {
+        Thread.sleep(sleepTime);
+        Actions actions = new Actions(remoteWebDriver);
+        WebElement clickSpace = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        actions.doubleClick(clickSpace).perform();
+
+        return Optional.ofNullable(clickSpace);
+
     }
 
     public Optional<WebElement> findElementByXpathAndSendKeys(Long sleepTime, Long wait, String xpath, Keys keys, RemoteWebDriver remoteWebDriver) throws InterruptedException {
@@ -108,5 +119,20 @@ public class SeleniumHelperComponent {
         return Optional.ofNullable(clickBox);
     }
 
+    public String findElementByXpathAndGetCssValue(Long sleepTime, Long wait, String xpath, String cssValue, RemoteWebDriver remoteWebDriver) throws InterruptedException {
+        Thread.sleep(sleepTime);
+        WebElement cssValueFromElement = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+
+        return cssValueFromElement.getCssValue(cssValue);
+    }
+
+    public String findElementByXpathAnGetText(Long sleepTime, Long wait, String xpath, RemoteWebDriver remoteWebDriver) throws InterruptedException {
+        Thread.sleep(sleepTime);
+        WebElement textValueFromElement = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(wait))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+
+        return textValueFromElement.getText();
+    }
 
 }
