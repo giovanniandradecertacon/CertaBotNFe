35ce573833a2a1fe1100c51a05a11b4f8db2c92d
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-28 16:43:06 -0300

    chore: criação de testes para CRUD de NfeFile

--- src/test/java/br/com/certacon/certabotnfefiles/services/NfeFileServiceTest.java
+++ src/test/java/br/com/certacon/certabotnfefiles/services/NfeFileServiceTest.java
@@ -1,39 +1,36 @@
 package br.com.certacon.certabotnfefiles.services;
 
-import br.com.certacon.certabotnfefiles.components.CreateNfeFileComponent;
 import br.com.certacon.certabotnfefiles.models.NfeFileModel;
 import br.com.certacon.certabotnfefiles.repositories.NfeFileRepository;
-import br.com.certacon.certabotnfefiles.vos.NfeFileForCreateVO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.openqa.selenium.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
 
-@SpringBootTest(classes = {NfeFileService.class, CreateNfeFileComponent.class})
+@SpringBootTest(classes = {NfeFileService.class})
 class NfeFileServiceTest {
     @MockBean
     NfeFileRepository fileRepository;
     @Autowired
     private NfeFileService fileService;
 
-    @Autowired
-    private CreateNfeFileComponent createComponent;
-
     private NfeFileModel fileModel;
 
     @BeforeEach
     void setUp() {
-        fileService = new NfeFileService(fileRepository, createComponent);
+        fileService = new NfeFileService(fileRepository);
 
         fileModel = NfeFileModel.builder()
                 .id(UUID.randomUUID())
@@ -49,30 +46,59 @@ class NfeFileServiceTest {
 
     @AfterEach
     void tearDown() {
-        fileService = null;
     }
 
     @Test
-    @DisplayName("chamar o metodo create quando Retornar com Sucesso")
+    @DisplayName("chamar o metodo saveOrUpdate quando Retornar com Sucesso")
     void shouldCallNfeFileServiceWhenCreateReturnWithSuccess() {
-        //Given
-        NfeFileForCreateVO fileForCreateVO = NfeFileForCreateVO.builder()
-                .fileName(fileModel.getFileName())
-                .remoteDriverUpload(fileModel.getRemoteDriverUpload())
-                .pathToFile(fileModel.getPathToFile())
-                .cnpj(fileModel.getCnpj())
-                .nomeEmpresa(fileModel.getNomeEmpresa())
-                .username(fileModel.getUsername())
-                .password(fileModel.getPassword())
-                .build();
-
-        NfeFileModel expected = NfeFileModel.builder()
-                .cnpj("Certacon")
-                .build();
         //When
-        BDDMockito.when(fileRepository.save(any(NfeFileModel.class))).thenReturn(expected);
-        NfeFileModel actual = fileService.create(fileForCreateVO);
+        BDDMockito.when(fileRepository.save(any(NfeFileModel.class))).thenReturn(fileModel);
+        NfeFileModel actual = fileService.saveOrUpdate(fileModel);
         //Then
-        assertEquals(expected.getCnpj(), actual.getId());
+        assertEquals(fileModel.getCnpj(), actual.getCnpj());
+    }
+
+    @Test
+    @DisplayName("chamar o metodo saveOrUpdate quando Retornar com Erro")
+    void shouldCallNfeFileServiceWhenCreateReturnWithError() {
+        //Given
+        NfeFileModel nfeFileModel = NfeFileModel.builder()
+                .id(UUID.randomUUID())
+                .nomeEmpresa("Certacon")
+                .cnpj("00.181.904/0001-97")
+                .username("giovanni.andrade@certacon.com.br")
+                .pathToFile("D:\\loadFileData\\192168062\\06333120000197\\2023\\NFe")
+                .fileName("TesteFile.zip")
+                .password("1")
+                .remoteDriverUpload("http://192.168.0.62/tributario")
+                .build();
+        IllegalArgumentException expected = new IllegalArgumentException("File name cannot be null or empty");
+        //When
+        BDDMockito.when(fileRepository.save(any(NfeFileModel.class))).thenReturn(nfeFileModel);
+        IllegalArgumentException actual = assertThrows(IllegalArgumentException.class, () -> fileService.saveOrUpdate(nfeFileModel));
+        //Then
+        assertEquals(expected.getMessage(), actual.getMessage());
+    }
+
+    @Test
+    @DisplayName("chamar o metodo DeleteFile quando retornar com Sucesso")
+    void shouldCallNfeFileServiceWhenDeleteFileReturnWithSuccess() {
+        //When
+        BDDMockito.when(fileRepository.findById(any(UUID.class))).thenReturn(Optional.of(fileModel));
+        Boolean actual = fileService.deleteFile(fileModel.getId());
+        //Then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("chamar o metodo DeleteFile quando retornar com Erro")
+    void shouldCallNfeFileServiceWhenDeleteFileReturnWithNotFoundError() {
+        //Given
+        NotFoundException expected = new NotFoundException("Arquivo não encontrado");
+        //When
+        BDDMockito.when(fileRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+        NotFoundException actual = assertThrows(NotFoundException.class, () -> fileService.deleteFile(fileModel.getId()));
+        //Then
+        assertEquals(expected.getMessage(), actual.getMessage());
     }
 }
