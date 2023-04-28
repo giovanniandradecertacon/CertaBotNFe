ffc834f70926988abd8df60db5157fe239933388
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-28 16:42:12 -0300

    chore: criação de metodo findByFilename

--- src/main/java/br/com/certacon/certabotnfefiles/repositories/NfeFileRepository.java
+++ src/main/java/br/com/certacon/certabotnfefiles/repositories/NfeFileRepository.java
@@ -4,8 +4,10 @@ import br.com.certacon.certabotnfefiles.models.NfeFileModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NfeFileRepository extends JpaRepository<NfeFileModel, UUID> {
+    Optional<NfeFileModel> findByFileName(String fileName);
 }
