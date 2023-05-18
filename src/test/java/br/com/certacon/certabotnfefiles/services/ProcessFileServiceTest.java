6180c48153b2194e1cbee41d03be4d219fce2cde
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-18 11:53:20 -0300

    test: created test for process file services

---
+++ src/test/java/br/com/certacon/certabotnfefiles/services/ProcessFileServiceTest.java
@@ -0,0 +1,7 @@
+package br.com.certacon.certabotnfefiles.services;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {})
+public class ProcessFileServiceTest {
+}
