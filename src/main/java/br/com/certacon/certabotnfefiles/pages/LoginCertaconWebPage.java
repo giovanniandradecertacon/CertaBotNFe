40bece555cc4d0c51ea2ad238f58d4bcdb40fece
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-26 17:45:21 -0300

    chore: implemented error message treatment

--- src/main/java/br/com/certacon/certabotnfefiles/pages/LoginCertaconWebPage.java
+++ src/main/java/br/com/certacon/certabotnfefiles/pages/LoginCertaconWebPage.java
@@ -5,11 +5,15 @@ import br.com.certacon.certabotnfefiles.models.ProcessFileModel;
 import br.com.certacon.certabotnfefiles.repositories.ProcessFileRepository;
 import br.com.certacon.certabotnfefiles.utils.ProcessStatus;
 import br.com.certacon.certabotnfefiles.vos.ProcessFileForLoginVO;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Optional;
 
 @Component
@@ -36,6 +40,17 @@ public class LoginCertaconWebPage {
                     Optional<WebElement> passwordElement = helper.findElementByXpathAndSendKeysString(1000L, 30L, "/html/body/div[3]/div[2]/div/div[2]/div[2]/div/input", processFileVO.getPassword(), remoteWebDriver);
                     if (passwordElement.isPresent()) {
                         Optional<WebElement> loginElement = helper.findElementByXpathAndSendKeys(1000L, 30L, "/html/body/div[3]/div[2]/div/div[2]/div[3]/a", Keys.ENTER, remoteWebDriver);
+
+                        Optional<WebElement> errorMessage = helper.findByXpathAndSendKeysForError(10L, remoteWebDriver, "class^=[modal fade show]");
+                        if (errorMessage.isPresent()) {
+                            WebElement errorMessageButton = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(10))
+                                    .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/button")));
+                            errorMessageButton.click();
+                            helper.findElementByIdWithSendKeys(1000L, 30L, "geo-login-name", processFileVO.getUsername(), remoteWebDriver);
+                            helper.findElementByXpathAndSendKeysString(1000L, 30L, "/html/body/div[3]/div[2]/div/div[2]/div[2]/div/input", processFileVO.getPassword(), remoteWebDriver);
+                            helper.findElementByXpathAndSendKeys(1000L, 30L, "/html/body/div[3]/div[2]/div/div[2]/div[3]/a", Keys.ENTER, remoteWebDriver);
+                        }
+
                         if (loginElement.isPresent()) loginStatus = ProcessStatus.LOGGED;
                     }
                 }
